64d6298abba0bc5be25c3b0043a50c06bc6daba4
Amirah Rizkita Setiadji <amirahsetiadji@gmail.com>
2024-08-29 06:09:16 +0700

    Add URL deplow pws

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x9d2+=50530(-kme-eh+w$=)dg@!uc)uffiea*-9jn!wl$qmal
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "amirah-rizkita-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
